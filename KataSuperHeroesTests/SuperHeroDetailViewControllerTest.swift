26454d6f5ef6c9c2441f0dd17215cae5e5492703
Óscar Manrique <oscar.manrique@vass.es>
2017-11-20 18:36:25 +0100

    test de la descripción en el detalle de un héroe

---
+++ KataSuperHeroesTests/SuperHeroDetailViewControllerTest.swift
@@ -0,0 +1,43 @@
+//
+//  SuperHeroDetailViewControllerTest.swift
+//  KataSuperHeroesTests
+//
+//  Created by Óscar Manrique on 20/11/17.
+//  Copyright © 2017 GoKarumi. All rights reserved.
+//
+
+import Foundation
+import KIF
+import Nimble
+import UIKit
+@testable import KataSuperHeroes
+
+class SuperHeroDetailViewControllerTest: AcceptanceTestCase {
+    
+    fileprivate let repository = MockSuperHeroesRepository()
+    fileprivate let superHero = SuperHero(name: "Eto",
+        photo: NSURL(string: "https://i.annihil.us/u/prod/marvel/i/mg/c/60/55b6a28ef24fa.jpg") as URL?,
+        isAvenger: true, description: "Description - Eto dfg gvasdg adsgaksdlga sdfaskljaslkdf asdf ")
+    
+
+    func testShowDescriptionIfThereLoadSuperHero() {
+        openSuperHeroDetailViewController()
+        
+        tester().waitForView(withAccessibilityLabel: "Description: \(superHero.name)")
+    }
+    
+    
+    fileprivate func openSuperHeroDetailViewController() {
+        
+        let superHeroName = superHero.name
+        repository.superHeroes = [superHero]
+        let superHeroDetailViewController = ServiceLocator().provideSuperHeroDetailViewController(superHeroName)
+            as! SuperHeroDetailViewController
+        superHeroDetailViewController.presenter = SuperHeroDetailPresenter(ui: superHeroDetailViewController, superHeroName: superHeroName, getSuperHeroByName: GetSuperHeroByName(repository: repository))
+        
+        let rootViewController = UINavigationController()
+        rootViewController.viewControllers = [superHeroDetailViewController]
+        present(viewController: rootViewController)
+        tester().waitForAnimationsToFinish()
+    }
+}
